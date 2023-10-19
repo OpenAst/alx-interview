89938ed69128093ca1720622a3a5e9b5e8f99f0b
OpenAst <isrealme0@gmail.com>
2023-10-19 10:24:57 -0700

    work done

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -2,53 +2,42 @@
 
 import sys
 
-
-def print_msg(dict_sc, total_file_size):
-    """
-    Method to print
-    Args:
-        dict_sc: dict of status codes
-        total_file_size: total of the file
-    Returns:
-        Nothing
-    """
-
-    print("File size: {}".format(total_file_size))
-    for key, val in sorted(dict_sc.items()):
-        if val != 0:
-            print("{}: {}".format(key, val))
-
-
-total_file_size = 0
-code = 0
-counter = 0
-dict_sc = {"200": 0,
-           "301": 0,
-           "400": 0,
-           "401": 0,
-           "403": 0,
-           "404": 0,
-           "405": 0,
-           "500": 0}
+TOTAL_FILE_SIZE = 0
+STATUS_CODE_COUNTS = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+LINE_COUNT = 0
 
 try:
     for line in sys.stdin:
-        parsed_line = line.split()  # ✄ trimming
-        parsed_line = parsed_line[::-1]  # inverting
+        parts = line.split()
+        if len(parts) != 7:
+            continue
 
-        if len(parsed_line) > 2:
-            counter += 1
+        ip, _, _, _, _, status_code, file_size = parts
 
-            if counter <= 10:
-                total_file_size += int(parsed_line[0])  # file size
-                code = parsed_line[1]  # status code
+        try:
+            status_code = int(status_code)
+            if status_code in STATUS_CODE_COUNTS:
+                STATUS_CODE_COUNTS[status_code] += 1
+                TOTAL_FILE_SIZE += int(file_size)
+                LINE_COUNT += 1
+        except ValueError:
+            pass
 
-                if (code in dict_sc.keys()):
-                    dict_sc[code] += 1
+        if LINE_COUNT >= 10:
+            print(f"Total file size: File size: {TOTAL_FILE_SIZE}")
+            for code, count in sorted(STATUS_CODE_COUNTS.items()):
+                if count > 0:
+                    print(f"{code}: {count}")
+            print("----")
+            LINE_COUNT = 0
 
-            if (counter == 10):
-                print_msg(dict_sc, total_file_size)
-                counter = 0
+except KeyboardInterrupt:
+    print("Total file size: File size:", TOTAL_FILE_SIZE)
+    for code, count in sorted(STATUS_CODE_COUNTS.items()):
+        if count > 0:
+            print(f"{code}: {count}")
 
-finally:
-    print_msg(dict_sc, total_file_size)
+print("Total file size: File size:", TOTAL_FILE_SIZE)
+for code, count in sorted(STATUS_CODE_COUNTS.items()):
+    if count > 0:
+        print(f"{code}: {count}")
