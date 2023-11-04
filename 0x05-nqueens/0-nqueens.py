6fed17f1df59e1998f9227a40ccd0cea159e27eb
OpenAst <isrealme0@gmail.com>
2023-11-03 17:59:50 -0700

    updated

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -3,42 +3,74 @@
 import sys
 
 
-if len(sys.argv) > 2 or len(sys.argv) < 2:
-    print("Usage: nqueens N")
-    exit(1)
+def is_safe(board, row, col, N):
+    """Check if it is safe to place a queen at a 
+    particular position
+    """
+    for i in range(row):
+        if board[i][col] == 1:
+            return False
 
-if not sys.argv[1].isdigit():
-    print("N must be a number")
-    exit(1)
-
-if int(sys.argv[1]) < 4:
-    print("N must be at least 4")
-    exit(1)
-
-n = int(sys.argv[1])
+    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
 
 
-def queens(n, i=0, a=[], b=[], c=[]):
-    """ find possible positions """
-    if i < n:
-        for j in range(n):
-            if j not in a and i + j not in b and i - j not in c:
-                yield from queens(n, i + 1, a + [j], b + [i + j], c + [i - j])
-    else:
-        yield a
+    for i, j in zip(range(row, -1, -1), range(col, N)):
+        if board[i][j] == 1:
+            return False
+
+    return True
 
 
-def solve(n):
-    """ solve """
-    k = []
-    i = 0
-    for solution in queens(n, 0):
-        for s in solution:
-            k.append([i, s])
-            i += 1
-        print(k)
-        k = []
-        i = 0
+def solveNQueensUtil(board, row, N, solutions):
+    """ Utility function for N-Queens puzzle """
+    if row >= N:
+        solution = []
+        for i in range(N):
+            for j in range(N):
+                if board[i][j] == 1:
+                    solution.append([i, j])
+        solutions.append(solution)
+        return 
+
+    for col in range(N):
+        if is_safe(board, row, col, N):
+            board[row][col] = 1
+            solveNQueensUtil(board, row + 1, N, solutions)
+            board[row][col] = 0
+
+    return False
 
 
-solve(n)
+def solveNQueens(N):
+    """ Solves the NQueen puzzle """
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        exit(1)
+    
+    try:
+        N = int(N)
+    except ValueError:
+        print("N msut be a number")
+        exit(1)
+    if N < 4:
+        print("N must be at least 4")
+        exit(1)
+
+    board = [[0 for _ in range(N)] for _ in range(N)]
+
+    solutions = []
+
+    solveNQueensUtil(board, 0, N, solutions)
+
+    for solution in solutions:
+        print(solution)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        exit(1)
+
+    N = sys.argv[1]
+    solveNQueens(N)
