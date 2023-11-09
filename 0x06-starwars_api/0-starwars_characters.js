30bbd448fd36492808b469ecb66cb1507a5011a0
OpenAst <isrealme0@gmail.com>
2023-11-09 14:12:32 -0800

    done

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,17 +1,26 @@
 #!/usr/bin/node
 
 const request = require('request');
+const movieId = process.argv[2];
 
-request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, res, body) {
-  if (err) throw err;
-  const actors = JSON.parse(body).characters;
-  exactOrder(actors, 0);
+const apiUrl 'https://swapi-api.alx-tools.com/api/films/${movieId}/';
+
+request(apiUrl, (error, response, body) => {
+  if (!error && response.statusCode == 200) {
+    const filmData = JSON.parse(body);
+    const characters = filmData.characters;
+
+    characters.forEach(characterUrl => {
+      request(characterUrl, (charError, charResponse, charBody) => {
+        if (!charError && charResponse.statusCode === 200) {
+          const characterData = JSON.parse(charBody);
+          console.log(characterData.name);
+        } else {
+          console.error('Error fetching character data:', charError);
+        }
+      });
+    });
+  } else {
+    console.error('Error fetching movie data:', error);
+  }
 });
-const exactOrder = (actors, x) => {
-  if (x === actors.length) return;
-  request(actors[x], function (err, res, body) {
-    if (err) throw err;
-    console.log(JSON.parse(body).name);
-    exactOrder(actors, x + 1);
-  });
-};
