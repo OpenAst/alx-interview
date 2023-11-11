3d281539da9273474b0b6304d8e5679799984977
OpenAst <isrealme0@gmail.com>
2023-11-11 03:48:55 -0800

    updated

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,13 +1,27 @@
 #!/usr/bin/node
 
+; 'use strict';
+
 const request = require('request');
 
-function getMovieCharacters(movieId) {
+function getCharacterName(characterUrl) {
+  return new Promise((resolve, reject) => {
+    request.get({ url: characterUrl, json: true }, (error, response, characterData) => {
+      if (error) {
+        reject(`Error fetching character data: ${error.message}`);
+      } else {
+        resolve(characterData.name);
+      }
+    });
+  });
+}
+
+async function getMovieCharacters(movieId) {
   // Star Wars API base URL
   const baseUrl = 'https://swapi.dev/api/films/';
 
   // Make a request to get information about the specified movie
-  request.get({ url: `${baseUrl}${movieId}/`, json: true }, (error, response, movieData) => {
+  request.get({ url: `${baseUrl}${movieId}/`, json: true }, async (error, response, movieData) => {
     if (error) {
       console.error(`Error fetching movie data: ${error.message}`);
       return;
@@ -16,29 +30,17 @@ function getMovieCharacters(movieId) {
     // Extract characters list from the movie data
     const charactersUrls = movieData.characters;
 
-    // Use a recursive function to process characters one by one
-    function processCharacter(index) {
-      if (index === charactersUrls.length) {
-        // All characters processed, exit
-        return;
-      }
+    try {
+      // Use Promise.all to wait for all character requests to complete
+      const characterNames = await Promise.all(charactersUrls.map(getCharacterName));
 
-      // Make a request to get information about the character
-      request.get({ url: charactersUrls[index], json: true }, (characterError, characterResponse, characterData) => {
-        if (characterError) {
-          console.error(`Error fetching character data: ${characterError.message}`);
-        } else {
-          // Print the character name
-          console.log(characterData.name);
-        }
-
-        // Process the next character
-        processCharacter(index + 1);
+      // Print character names
+      characterNames.forEach(name => {
+        console.log(name);
       });
+    } catch (error) {
+      console.error(`Error: ${error}`);
     }
-
-    // Start processing characters from index 0
-    processCharacter(0);
   });
 }
 
