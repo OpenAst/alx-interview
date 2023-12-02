168d3c1ad266ca8541406982d591493f1763215a
OpenAst <isrealme0@gmail.com>
2023-12-02 01:48:57 -0800

    done

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Compute the perimeter of a grid
+"""
+
+def island_perimeter(grid):
+    """Compute the perimeter of a grid."""
+    if not grid:
+        return 0
+
+    perimeter = 0
+    rows = len(grid)
+    cols = len(grid[0])
+
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == 1:
+                perimeter += 4  # Assume all sides are surrounded by water
+
+                # Check neighboring cells, subtract perimeter for adjacent land
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 2  # Subtract left side
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 2  # Subtract top side
+
+    return perimeter
+
