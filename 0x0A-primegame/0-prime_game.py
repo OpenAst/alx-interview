b3601a06eae069e3836dbc928afc15efb71197fd
OpenAst <isrealme0@gmail.com>
2023-12-08 10:39:09 -0800

    Tasks done

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,57 +1,46 @@
-#!/usr/bin/python3
-"""Module defining isWinner function."""
-
-
 def isWinner(x, nums):
-    """Function to get who has won in prime game"""
-    mariaWinsCount = 0
-    benWinsCount = 0
-
-    for num in nums:
-        roundsSet = list(range(1, num + 1))
-        primesSet = primes_in_range(1, num)
-
-        if not primesSet:
-            benWinsCount += 1
-            continue
-
-        isMariaTurns = True
-
-        while(True):
-            if not primesSet:
-                if isMariaTurns:
-                    benWinsCount += 1
-                else:
-                    mariaWinsCount += 1
-                break
-
-            smallestPrime = primesSet.pop(0)
-            roundsSet.remove(smallestPrime)
-
-            roundsSet = [x for x in roundsSet if x % smallestPrime != 0]
-
-            isMariaTurns = not isMariaTurns
-
-    if mariaWinsCount > benWinsCount:
-        return "Winner: Maria"
-
-    if mariaWinsCount < benWinsCount:
-        return "Winner: Ben"
-
-    return None
-
-
-def is_prime(n):
-    """Returns True if n is prime, else False."""
-    if n < 2:
-        return False
-    for i in range(2, int(n ** 0.5) + 1):
-        if n % i == 0:
+    # Function to check if a number is prime
+    def is_prime(num):
+        if num < 2:
             return False
-    return True
+        for i in range(2, int(num ** 0.5) + 1):
+            if num % i == 0:
+                return False
+        return True
 
+    # Function to simulate the game for a particular round
+    def play_game(n):
+        primes = [i for i in range(2, n + 1) if is_prime(i)]
+        primes_set = set(primes)
+        turn = 1  # Maria's turn is denoted by 1, Ben's turn is denoted by 2
+        while primes_set:
+            playable = [num for num in primes_set if num <= n]
+            if not playable:
+                break
+            if turn == 1:  # Maria's turn
+                for num in playable:
+                    primes_set -= set(range(num, n + 1, num))
+                turn = 2
+            else:  # Ben's turn
+                for num in playable:
+                    primes_set -= set(range(num, n + 1, num))
+                turn = 1
+        return turn == 1  # If it's Maria's turn, Ben wins; otherwise, Maria wins
 
-def primes_in_range(start, end):
-    """Returns a list of prime numbers between start and end (inclusive)."""
-    primes = [n for n in range(start, end+1) if is_prime(n)]
-    return primes
+    # Count the number of wins for Maria and Ben
+    maria_wins = 0
+    ben_wins = 0
+    for n in nums:
+        if play_game(n):
+            maria_wins += 1
+        else:
+            ben_wins += 1
+
+    # Determine the winner with the most rounds won
+    if maria_wins > ben_wins:
+        return "Maria"
+    elif maria_wins < ben_wins:
+        return "Ben"
+    else:
+        return None
+
