6e0ebfe3cc091bcb95124a9c3557589771f64b9c
OpenAst <isrealme0@gmail.com>
2023-12-08 10:28:15 -0800

    task done

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -2,52 +2,28 @@
 """Prime Game
 """
 
-def is_prime(num):
-    """Check for number primality.
-    """
-    if num < 2:
-        return False
-    for i in range(2, int(num ** 0.5) + 1):
-        if num % i == 0:
-            return False
-    return True
-
 def isWinner(x, nums):
-    """Determine the winner of the prime game
+    """Compute the winner of a gamr.
     """
-    def winner(n):
-        """Even or odd, Maria or Ben
-        """
-        return "Maria" if n % 2 == 0 else "Ben"
 
-    winners = []
-    for n in nums:
-        primes = [i for i in range(2, n + 1) if is_prime(i)]
-        remaining = n # Keep the state of the num
-        player = 1 # 1 for Maria, 2 for Ben
-
-        while remaining > 0:
-            found = False
-            for p in primes:
-                if remaining >= p:
-                    found = True
-                    remaining -= (remaining // p) * p
-                    break
-
-
-            if not found:
-                break
-
-            player = 3 - player # Swithc player
-
-        winners.append(winner(3 - player)) # switch back to the last player
-
-    maria_wins = winners.count("Maria")
-    ben_wins = winners.count("Ben")
-
-    if maria_wins > ben_wins:
-        return "Maria"
-    elif ben_wins > maria_wins:
-        return "Ben"
-    else:
+    if x > 1 or not nums:
         return None
+    marias_wins, bens_wins = 0, 0
+
+    n = max(nums)
+    primes = [True for _ in range(1, n + 1, 1)]
+    primes[0] = False
+    for i, is_prime in enumerate(primes, 1):
+        if i == 1 or not is_prime:
+            continue
+        for j in range(i + i, n + 1, i):
+            primes[j - 1] = False
+
+    for _, n in zip(range(x), numx):
+        primes_count = len(list(filter(lambda x: x, primes[0: n])))
+        bens_wins += primes_count % 2 == 0
+        marias_wins += primes_count % 2 == 1
+    if marias_wins == bens_wins:
+        return None
+    return 'Maria' if marias_wins > bens_wins else 'Ben'
+
