634d0d01fcb1758a0f7dfc17715b09968c5b826c
OpenAst <isrealme0@gmail.com>
2023-12-11 00:33:08 -0800

    done

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,46 +1,79 @@
+#!/usr/bin/python3
+"""Module for Prime Game"""
+
+
 def isWinner(x, nums):
-    # Function to check if a number is prime
-    def is_prime(num):
-        if num < 2:
-            return False
-        for i in range(2, int(num ** 0.5) + 1):
-            if num % i == 0:
-                return False
-        return True
+    """
+    Determines the winner of a set of prime number removal games.
 
-    # Function to simulate the game for a particular round
-    def play_game(n):
-        primes = [i for i in range(2, n + 1) if is_prime(i)]
-        primes_set = set(primes)
-        turn = 1  # Maria's turn is denoted by 1, Ben's turn is denoted by 2
-        while primes_set:
-            playable = [num for num in primes_set if num <= n]
-            if not playable:
-                break
-            if turn == 1:  # Maria's turn
-                for num in playable:
-                    primes_set -= set(range(num, n + 1, num))
-                turn = 2
-            else:  # Ben's turn
-                for num in playable:
-                    primes_set -= set(range(num, n + 1, num))
-                turn = 1
-        return turn == 1  # If it's Maria's turn, Ben wins; otherwise, Maria wins
+    Args:
+        x (int): The number of rounds.
+        nums (list of int): A list of integers where each integer n denotes
+        a set of consecutive integers starting from 1 up to and including n.
 
-    # Count the number of wins for Maria and Ben
-    maria_wins = 0
-    ben_wins = 0
-    for n in nums:
-        if play_game(n):
-            ben_wins += 1
-        else:
-            maria_wins += 1
+    Returns:
+        str: The name of the player who won the most rounds (either "Ben"
+        or "Maria").
+        None: If the winner cannot be determined.
 
-    # Determine the winner with the most rounds won
-    if maria_wins > ben_wins:
-        return "Maria"
-    elif maria_wins < ben_wins:
-        return "Ben"
-    else:
+    Raises:
+        None.
+    """
+    # Check for invalid input
+    if x <= 0 or nums is None:
         return None
+    if x != len(nums):
+        return None
+    # Initialize scores and array of possible prime numbers
+    ben = 0
+    maria = 0
+    # Create a list 'a' of length sorted(nums)[-1] + 1 with all elements
+    # initialized to 1
+    a = [1 for x in range(sorted(nums)[-1] + 1)]
+    # The first two elements of the list, a[0] and a[1], are set to 0
+    # because 0 and 1 are not prime numbers
+    a[0], a[1] = 0, 0
+    # Use Sieve of Eratosthenes algorithm to generate array of prime numbers
+    for i in range(2, len(a)):
+        rm_multiples(a, i)
+    # Play each round of the game
+    for i in nums:
+        # If the sum of prime numbers in the set is even, Ben wins
+        if sum(a[0:i + 1]) % 2 == 0:
+            ben += 1
+        else:
+            maria += 1
+    # Determine the winner of the game
+    if ben > maria:
+        return "Ben"
+    if maria > ben:
+        return "Maria"
+    return None
 
+
+def rm_multiples(ls, x):
+    """
+    Removes multiples of a prime number from an array of possible prime
+    numbers.
+
+    Args:
+        ls (list of int): An array of possible prime numbers.
+        x (int): The prime number to remove multiples of.
+
+    Returns:
+        None.
+
+    Raises:
+        None.
+    """
+    # This loop iterates over multiples of a prime number and marks them as
+    # non-prime by setting their corresponding value to 0 in the input
+    # list ls. Starting from 2, it sets every multiple of x up to the
+    # length of ls to 0. If the index i * x is out of range for the list ls,
+    # the try block will raise an IndexError exception, and the loop will
+    # terminate using the break statement.
+    for i in range(2, len(ls)):
+        try:
+            ls[i * x] = 0
+        except (ValueError, IndexError):
+            break
